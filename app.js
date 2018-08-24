89ef6f04b3361efc781d03e9c03feb15d7af66d0
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-08-24 17:53:01 +0200

    Added two distinct instances

---
+++ app.js
@@ -0,0 +1,16 @@
+const helloWorld = new Vue({
+    el: '#helloVue',
+    data: {
+        title: 'Hello World',
+        message: 'This is my first vue template !!!!'
+    }
+});
+
+const example = new Vue({
+    el: '#example',
+    data: {
+        title: 'Hello',
+        message: 'This is your daily cat information!',
+        name: 'Chewie'
+    }
+});
