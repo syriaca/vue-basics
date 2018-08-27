cc7e79274289ff94d2847ac5a88dd1c884c064e8
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-08-27 18:10:59 +0200

    Movie vue (add interactivity)

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-const helloWorld = new Vue({
-    el: '#helloVue',
+const book = new Vue({
+    el: '#book',
     data: {
-        title: 'Hello World',
-        message: 'This is my first vue template !!!!'
-    }
-});
-
-const example = new Vue({
-    el: '#example',
-    data: {
-        title: 'Hello',
-        message: 'This is your daily cat information!',
-        name: 'Chewie'
+      title: 'The Sirens of Titan',
+      author: 'Kurt Vonnegut',
+      summary: 'This is a summary of the Sirens of Titan.',
+      showDetail: false
     }
 });
